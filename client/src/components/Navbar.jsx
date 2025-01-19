06ed83d020614d73dda24a57d8225ee65565335b
Casperk21 <casper.hvid.kristensen@gmail.com>
2025-01-19 18:07:21 +0100

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/navbar.css'; // Importér CSS til navigationen
+import '../styles/navbar.css'; // Importér CSS
 
-const Navbar = () => (
-  <nav>
-    <div>
-      <Link to="/">Hjem</Link>
-      <Link to="/flyttebilberegner">Flyttebilberegner</Link>
-      <Link to="/tjekliste">Tjekliste</Link>
-      <Link to="/3d-visualisering">3D-Visualisering</Link>
-      <Link to="/flytteguide">Flytteguide</Link>
-      <Link to="/om-os">Om os</Link>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  return (
+    <nav>
+      <div className="nav-links">
+        <Link to="/">Hjem</Link>
+        <Link to="/flyttebilberegner">Flyttebilberegner</Link>
+        <Link to="/tjekliste">Tjekliste</Link>
+        <div className="dropdown">
+          <button className="dropdown-toggle" onClick={toggleDropdown}>
+            Mere
+          </button>
+          {dropdownOpen && (
+            <div className="dropdown-menu">
+              <Link to="/3d-visualisering">3D-Visualisering</Link>
+              <Link to="/flytteguide">Flytteguide</Link>
+              <Link to="/om-os">Om os</Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;

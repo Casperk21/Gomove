9dd1457eb40d700d52a7551426f2517ba2c2b35a
Casperk21 <casper.hvid.kristensen@gmail.com>
2025-01-19 15:30:30 +0100

    Create Flytteguide.jsx

---
+++ client/src/pages/Flytteguide.jsx
@@ -0,0 +1,15 @@
+const Flytteguide = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Flytteguide</h1>
+      <p>Her finder du gode råd og vejledning til at planlægge din flytning.</p>
+      <ul className="list-disc pl-6 mt-4">
+        <li>Planlæg din flytning mindst 1 måned i forvejen.</li>
+        <li>Pak tunge genstande i små kasser for at gøre dem lettere at bære.</li>
+        <li>Mærk dine kasser med rummene, de hører til.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Flytteguide;

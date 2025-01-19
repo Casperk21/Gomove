dca3c68aaa550088076185c0dcd1b036dc39c721
Casperk21 <casper.hvid.kristensen@gmail.com>
2025-01-19 17:25:09 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,15 @@
-import '../styles/home.css';
-import { Link } from "react-router-dom";
+import React from 'react';
+import '../styles/home.css'; // Importér CSS til forsiden
 
-const Home = () => {
-  return (
-    <section className="text-center py-20 bg-gray-100">
-      <h1 className="text-5xl font-bold">Velkommen til Gomove</h1>
-      <p className="mt-4 text-lg">Din komplette løsning til en effektiv flytning.</p>
-      <div className="mt-6 space-x-4">
-        <Link to="/flyttebilberegner" className="bg-blue-600 text-white px-4 py-2 rounded">Prøv Flyttebilberegner</Link>
-        <Link to="/tjekliste" className="bg-gray-300 px-4 py-2 rounded">Se Tjekliste</Link>
-      </div>
-    </section>
-  );
-};
+const Home = () => (
+  <div className="home-header">
+    <h1>Velkommen til Gomove</h1>
+    <p>Din komplette løsning til flytning.</p>
+    <div className="home-actions">
+      <a href="/flyttebilberegner">Start Beregning</a>
+      <a href="/tjekliste">Lav Tjekliste</a>
+    </div>
+  </div>
+);
 
 export default Home;

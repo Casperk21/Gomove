a9eb5db9df70b504e3b47c482ec70265522130e5
Casperk21 <casper.hvid.kristensen@gmail.com>
2025-01-19 17:10:36 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Flyttebilberegner from "./pages/Flyttebilberegner";
-import Visualisering from "./pages/Visualisering";
-import Tjekliste from "./pages/Tjekliste";
-import Flytteguide from "./pages/Flytteguide";
-import OmOs from "./pages/OmOs";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Flyttebilberegner from './pages/Flyttebilberegner';
+import Tjekliste from './pages/Tjekliste';
+import Visualisering from './pages/Visualisering';
+import Flytteguide from './pages/Flytteguide';
+import OmOs from './pages/OmOs';
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/flyttebilberegner" element={<Flyttebilberegner />} />
-        <Route path="/3d-visualisering" element={<Visualisering />} />
-        <Route path="/tjekliste" element={<Tjekliste />} />
-        <Route path="/flytteguide" element={<Flytteguide />} />
-        <Route path="/om-os" element={<OmOs />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/flyttebilberegner" element={<Flyttebilberegner />} />
+      <Route path="/tjekliste" element={<Tjekliste />} />
+      <Route path="/3d-visualisering" element={<Visualisering />} />
+      <Route path="/flytteguide" element={<Flytteguide />} />
+      <Route path="/om-os" element={<OmOs />} />
+    </Routes>
+  </Router>
+);
 
 export default App;

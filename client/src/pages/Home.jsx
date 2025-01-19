07aa8895c24ad48adc2cf797f917fe7393317767
Casperk21 <casper.hvid.kristensen@gmail.com>
2025-01-19 17:47:56 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import '../styles/home.css'; // Importér CSS til forsiden
+import '../styles/home.css'; // Importér CSS
 
 const Home = () => (
   <div className="home-header">
     <h1>Velkommen til Gomove</h1>
-    <p>Din komplette løsning til flytning.</p>
+    <p>
+      Gomove er din komplette løsning til en nem og effektiv flytning. Med vores
+      værktøjer kan du planlægge og organisere din flytning uden besvær.
+    </p>
     <div className="home-actions">
       <a href="/flyttebilberegner">Start Beregning</a>
       <a href="/tjekliste">Lav Tjekliste</a>
